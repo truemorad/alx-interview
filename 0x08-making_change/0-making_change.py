26d1495f42fdd19e2e2ba35b80e33a13a20347c6
truemorad <truemorad@gmail.com>
2024-11-27 02:28:05 +0400

    legendAgain

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,18 +2,13 @@
 
 
 def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    
     coins.sort(reverse=True)
-    i = 0
     numOfCoins = 0
-    while i < len(coins):
-        if total > 0:
-            if coins[i] <= total:
-                total -= coins[i]
-                numOfCoins += 1
-            else:
-                i += 1
-        elif total < 0:
-            return 0
-        else:
-            return numOfCoins
-    return -1
+    for i in coins:
+        while i <= total:
+            total -= i
+            numOfCoins += 1
+    return numOfCoins if total == 0 else -1
