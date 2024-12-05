2b74b4e650d74f0b19837c49390c98dbe0f2e9e8
truemorad <truemorad@gmail.com>
2024-12-06 00:55:52 +0400

    intial

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    sides = 0
+    for i in grid:
+        for j in i:
+            index = grid.index(i) - 1
+            if i[j] == 1:
+                if grid[index][j - 1] == 0:
+                    sides += 1
+                if grid[index][j + 1] == 0:
+                    sides += 1
+                if grid[index + 1][j] == 0:
+                    sides += 1
+                if grid[index - 1][j] == 0:
+                    sides += 1    
+    return 0 if sides == 0 else sides
