d382d9b61f06bf6cae0bf929fdfca5f0b80c1d2e
truemorad <truemorad@gmail.com>
2024-12-06 01:23:14 +0400

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,17 +14,17 @@ def island_perimeter(grid):
     """
     sides = 0
     index = len(grid)
-    number = len(grid[0])
+    numbers = len(grid[0])
     for i in range(index):
-        for j in range(number):
+        for j in range(numbers):
             if grid[i][j] == 1:
-                if grid[i][j - 1] == 0:
+                if j == numbers or grid[i][j - 1] == 0:
                     sides += 1
-                if grid[i][j + 1] == 0:
+                if j == numbers - 1 or grid[i][j + 1] == 0:
                     sides += 1
-                if grid[i + 1][j] == 0:
+                if i == index - 1 or grid[i + 1][j] == 0:
                     sides += 1
-                if grid[i - 1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     sides += 1
     return 0 if sides == 0 else sides
 
